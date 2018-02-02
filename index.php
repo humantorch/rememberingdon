ce0f769a39fd8902d42d790a1f5ffcc54752631a
Scott Kosman <kosman.scott@gmail.com>
2018-02-02 22:32:20 +0100

    initial commit of full site

---
+++ index.php
@@ -0,0 +1,85 @@
+<!-- -->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+
+
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html>
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>[ remembering don kosman ]</title>
+<style type="text/css">
+@import url(styles/dog.css);
+</style>
+<script type="text/javascript" src="https://media.dreamhost.com/ufo.js"></script>
+<script type="text/javascript" src="/scripts/external.js"></script>
+</head>
+
+
+<body>
+
+<div id="container">
+
+<div id="topblock">
+
+<ul>
+	<li><a href="#obit">obituary</a></li>
+	<li><a href="#photo">photo tribute</a></li>
+	<li><a href="#prog">memorial service programs</a></li>
+</ul>
+
+</div>
+<a name="obit"></a>
+<h2 id="obit"><span>obituary</span></h2>
+
+<p>On January 17, 2007, dearly loved and cherished husband, father, brother, uncle and friend, Don Kosman went to be with the Lord after a brief but very difficult battle with cancer.</p>
+
+<p>Don left behind his loving wife and soul mate Carol; daughter Marcey Kosman; son Scott (Shannon) Kosman, and of course his faithful spotted four legged companion &#8220;Phoenix&#8221;. Don is also survived by his sisters: Martha, Olga and Rose; his brothers: Bill, Chuck and John. He is predeceased by his parents Andrew and Annie Kocuipchyk; and three of his sisters: Mary, Catherine and Jennie. </p>
+
+<p>Don Kosman spent his career years in the Edmonton Public School system as a principal of junior high schools. When he retired eleven years ago, he did not lose his administrative touch, continuing his work out at Camp Nakamun, both a passion and a ministry for him. </p>
+
+<p>Although his battle with cancer claimed his life physically in the end, it did not destroy Don&#8217;s unwavering faith in God. It did not rob his endearing sense of humor. It did not mask his wisdom. Nor did it quench his endless generosity. He was a man full of love, joy and inner strength, committed to his passions, and loyal to his family, friends, and his God. A role model to many, and loved by all who knew him, he will be dearly missed. </p>
+
+<p>A very warm thank you to all the staff at the University of Alberta Hospital, and the Cross Cancer Institute for their wonderful care of our loved one. Thanks to all the people who sat with Don near the end, and gave of their time. You are loved. </p>
+
+<p>Memorial donations may be made to <a href="http://www.campnakamun.com" rel="external">Camp Nakamun</a>. </p>
+
+<p><em>Don's obituary was written by his daughter Marcey.</em></p>
+
+<a name="photo"></a>
+<h2 id="photo"><span>photo tribute</span></h2>
+<p id="dkosman-2007.flv"><a href="http://www.macromedia.com/go/getflashplayer">Get the Flash Player</a> to see this player.</p>
+ 
+<script type="text/javascript">
+  var FO = { movie:"https://media.dreamhost.com/mediaplayer.swf",width:"500",height:"375",majorversion:"7",build:"0",bgcolor:"#FFFFFF",
+             flashvars:"file=http://rememberingdon.com/media/dkosman-2007.flv&amp;showdigits=true&amp;autostart=false" };
+UFO.create(FO,"dkosman-2007.flv");
+</script>
+
+<p>This tribute was played at Don's memorial service. </p>
+
+<p><em>The slideshow was built by <a href="http://www.evolvingblue.com" rel="external">Taylor Reese</a>, and the accompanying music is </em>Be Thou My Vision<em> as performed by <a href="http://www.janetdrew.com" rel="external">Janet Drew</a>.</em> Viewing it requires the <a href="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash&promoid=BIOW" rel="external">Adobe Flash Player plugin</a>.</p>
+
+<a name="prog"></a>
+<h2 id="prog"><span>service program</span></h2>
+
+<p><a href="/media/program_web.pdf">Download the memorial service program</a>.</p>
+
+<p><em>The program was built by Don's son Scott. It requires the <a href="http://www.adobe.com/products/acrobat/readstep2.html" rel="external">Adobe Reader plugin</a> to view.</em></p>
+
+</div><!--/container-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-78430948-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+
+</html>
